8b33cae8922cefac0a13b2529bb25b9c32c80ac0
VIGNESH YANAMALAMANDA <vigneshyanamalamanda@gmail.com>
2025-04-10 20:51:01 +0000

    form.php created online with Bitbucket

---
+++ src/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WAPH-Login Page</title>
+</head>
+<body>
+    <h1>A Simple Login Form, WAPH</h1>
+    <h2>Please Sign In</h2>
+    <form action="index.php" method="POST" class="form login">
+        Username: <input type="text" class="text_field" name="username" required /> <br>
+        Password: <input type="password" class="text_field" name="password" required /> <br>
+        <button class="button" type="submit">Login</button>
+    </form>
+</body>
+</html>

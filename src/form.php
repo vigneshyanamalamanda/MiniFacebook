ed0cb9faf148c0fff7243f2d580dce88ff55be76
Upendra Dommaraju <uppiupendra13@gmail.com>
2025-04-23 02:40:00 +0000

    form.php created online with Bitbucket

--- src/form.php
+++ src/form.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WAPH-Login Page</title>
-</head>
-<body>
-    <h1>A Simple Login Form, WAPH</h1>
-    <h2>Please Sign In</h2>
-    <form action="index.php" method="POST" class="form login">
-        Username: <input type="text" class="text_field" name="username" required /> <br>
-        Password: <input type="password" class="text_field" name="password" required /> <br>
-        <button class="button" type="submit">Login</button>
-    </form>
-</body>
-</html>
+<?php
+// form.php - redirects to index.php which handles login logic
+header("Location: index.php");
+exit();
+

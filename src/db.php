8bf4672f4d087bd418bc94c09e6ab7e3786070ab
VIGNESH YANAMALAMANDA <vigneshyanamalamanda@gmail.com>
2025-04-23 02:34:28 +0000

    db.php created online with Bitbucket

---
+++ src/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$username = 'WAPHTEAM1';
+$password = 'teamproject';
+$dbname = 'waph_team';
+
+$mysqli = new mysqli($host, $username, $password, $dbname);
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+?>

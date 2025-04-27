7fa0003b42b522d4a37c70058f387901778175b5
vallabhaneni naga satya phanindra <vallabhanenin2@udayton.edu>
2025-04-28 02:49:48 +0530

    working repo commits

--- database/database-data.sql
+++ database/database-data.sql
@@ -2,5 +2,4 @@ drop table if exists users;
 create table users(
   username varchar(50) PRIMARY KEY,
   password varchar(100) NOT NULL);
-  
 INSERT INTO users(username,password) VALUES ('WAPHTEAM1',md5('teamproject'));
